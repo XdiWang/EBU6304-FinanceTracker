3be7ceb02bfbedffc1ddd3ed1abb509f2f40bcf8
Jessy-Chen <1072628348@qq.com>
2025-05-24 14:03:14 +0800

    Update LoginFrame.java

--- src/main/java/com/financetracker/view/LoginFrame.java
+++ src/main/java/com/financetracker/view/LoginFrame.java
@@ -15,7 +15,7 @@ import java.util.regex.Pattern; // 引入正则表达式相关的类
 /**
  * 登录窗口 - 允许用户登录或注册
  */
-public class LoginFrame extends JFrame {//new
+public class LoginFrame extends JFrame {//new branch
 
     private JTextField usernameField;
     private JPasswordField passwordField;
